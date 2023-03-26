712217914c1ab480d6cdb8eecb11a20a992fac95
BhushanShinde <bhushan787578@gmail.com>
2023-03-26 22:32:46 +0530

    java

---
+++ Lambda/Lambda/src/com/ani/lmd/App.java
@@ -0,0 +1,9 @@
+package com.ani.lmd;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("  Lambda Expressions ");
+        BasicDemo bd = new BasicDemo();
+        bd.demo9();
+    }
+}
---
+++ Lambda/Lambda/src/com/ani/lmd/BiConsumer.java
@@ -0,0 +1,5 @@
+package com.ani.lmd;
+
+public class BiConsumer<T1, T2> {
+
+}
